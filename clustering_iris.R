dc720942f961098a659041c7d93c07a903d61bb8
Shoaib Amini <shoaibamini@gmail.com>
2017-02-24 15:42:07 +0100

    Update clustering_iris.R

--- clustering_iris.R
+++ clustering_iris.R
@@ -1,6 +1,10 @@
-library(mclust)
-library(NMF)
+#This example script performes clustering on the iris dataset using k-means, hierarchical, model-based and non negative matrix factorization (nmf). 
+#The results are visualized using Principal Component Analysis (PCA).
 
+library(mclust) # Package for mobel-based clustering
+library(NMF) # Package for nmf
+
+#Preprocessing the iris dataset.
 data(iris)
 iris2=iris[,-5]
 iris.scale=scale(iris2)
@@ -9,11 +13,13 @@ iris.t=t(iris2)
 iris.dist=dist(iris2)
 k_range=seq(2,15)
 
+#Performes k-means clustering. The optimal number of clusters is determined using the silhouette value. 
 sil_kmeans=sapply(k_range,function (k) {mean(silhouette(kmeans(iris.scale,k,nstart=25)$cluster,iris.scale.dist)[,3])})
 sil_kmeans=c(c(0),sil_kmeans)
 kmeans_kmax=kmeans(iris.scale,which.max(sil_kmeans),nstart=25)
 sil_kmeans_kmax=silhouette(kmeans_kmax$cluster,iris.scale.dist)
 
+#Performes hierarchical clustering using 1-pearson correlation as distance measure. The optimal number of clusters is determined using the silhouette value.
 dissim=as.dist(1-cor(t(iris.scale)))
 hclust=hclust(dissim,method="ward.D2")
 sil_hclust=sapply(seq(2,15),function (x){mean(silhouette(cutree(hclust,k=x),dissim)[,3])})
@@ -21,27 +27,35 @@ sil_hclust=c(c(0),sil_hclust)
 hclust_kmax=cutree(hclust,k=which.max(sil_hclust))
 sil_hclust_kmax=silhouette(hclust_kmax,dissim)
 
+#Performes model-based clustering. The optimal number of clusters is determined using the bayesian information criterion
 MBclust=Mclust(iris.scale)
-sil_MBclust=silhouette(MBclust$classification,iris.scale.dist)
 
+#Performes non negative matrix factorization. The optimal number of clusters is determined using the silhouette value.
 k.max=15
 sil_nmf = rep(0,k.max)
+#Iterates through each k
 for (k in 2:k.max)
 {
+	#Performes nmf.
 	nmf_res<-nmf(iris.t, k , nrun=20)
+	#Calculates the silhouette value.
 	ss=silhouette(apply(coef(nmf_res), 2, which.max),iris.dist)
 	sil_nmf[k] = mean(ss[,3])
 
 }
-plot(sil_nmf,type='b')
+#Performes nmf and uses the k with the higest silhouette value.
 nmf_kmax=nmf(iris.t, which.max(sil_nmf) , nrun=20)
+#Extracts the cluster numbers.
 nmf_kmax_cluster=apply(coef(nmf_kmax), 2, which.max)
 sil_nmf_kmax=silhouette(nmf_kmax_cluster,iris.dist)
 
+#Performes PCA on the iris dataset
 pca=prcomp(iris.scale)
 
+#Combines the original iris data, the clustering results and the 1st and 2nd Principal Components.
 iris_cluster_pca=cbind(ID=c(1:nrow(iris.scale)),iris2,no_clust=rep(1,nrow(iris.scale)),kmeans=kmeans_kmax$cluster,hclust=hclust_kmax,MBclust=MBclust$classification,nmf=nmf_kmax_cluster,PC1=pca$x[,1],PC2=pca$x[,3])
 
+#Plots the clustering results using the 1st and 2nd Principal Components.
 pdf("clustering_vis.pdf")
 par(mfrow=c(4,3))
 plot(sil_kmeans,type='b')
@@ -62,7 +76,3 @@ plot(iris_cluster_pca$PC1,iris_cluster_pca$PC2,col=iris_cluster_pca$nmf)
 #dev.copy2pdf(file="clustering_vis.pdf")
 
 dev.off()
-
-write.table(iris_cluster_pca,file="clusterData.txt",sep="\t",quote=F,row.names=F)
-write.table(iris_cluster_pca[,4],file="testData.txt",sep="\t",quote=F,row.names=F,col.names=F)
-write.table(iris_cluster_pca[,1:5],file="newData.txt",sep="\t",quote=F,row.names=F)
