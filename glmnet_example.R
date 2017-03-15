7e3bf6e1cdb786a2d0ce29615a4baedcc56085eb
Shoaib Amini <shoaibamini@gmail.com>
2017-03-15 14:34:42 +0100

    Update glmnet_example.R

--- glmnet_example.R
+++ glmnet_example.R
@@ -11,20 +11,22 @@ library(glmnet)  # Package to fit ridge/lasso/elastic net models
 inputFileName="inputMatrix.txt"
 inputData=read.table(inputFileName,header=T)
 # Number of samples
-n=dim(inputData)[1]
+nrow=dim(inputData)[1]
 
 
 # Split data into train and test sets
-trainData=as.matrix(inputData[seq(1,0.7*n),])
-testData=as.matrix(inputData[-seq(1,0.7*n),])
+perTrainData=0.7*nrow
+randomRow=sample(seq(1,n),perTrainData)
+trainData=as.matrix(inputData[randomRow,])
+testData=as.matrix(inputData[-randomRow,])
 
 # Defining dependent and indepenent variable(s)
-dep_var=trainData[,1]
-indep_var=trainData[,-1]
+target=trainData[,1]
+features=trainData[,-1]
 
 # 10-fold Cross validation for each alpha = 0, 0.1, ... , 0.9, 1.0
 for (i in 0:10) {
-    assign(paste("fit", i, sep=""), cv.glmnet(indep_var,dep_var , type.measure="mse",
+    assign(paste("fit", i, sep=""), cv.glmnet(features,target , type.measure="mse",
                                               alpha=i/10,family="gaussian"))
 }
 
@@ -55,18 +57,6 @@ mse9 <- mean((testData[,1] - testDataPred9)^2)
 mse10 <- mean((testData[,1] - testDataPred10)^2)
 
 # Table containing alpha and MSE
-alpha_mse=NULL
-alpha_mse=rbind(alpha_mse,c(0.0,mse0))
-alpha_mse=rbind(alpha_mse,c(0.1,mse1))
-alpha_mse=rbind(alpha_mse,c(0.2,mse2))
-alpha_mse=rbind(alpha_mse,c(0.3,mse3))
-alpha_mse=rbind(alpha_mse,c(0.4,mse4))
-alpha_mse=rbind(alpha_mse,c(0.5,mse5))
-alpha_mse=rbind(alpha_mse,c(0.6,mse6))
-alpha_mse=rbind(alpha_mse,c(0.7,mse7))
-alpha_mse=rbind(alpha_mse,c(0.8,mse8))
-alpha_mse=rbind(alpha_mse,c(0.9,mse9))
-alpha_mse=rbind(alpha_mse,c(1.0,mse10))
-
-colnames(alpha_mse)=c("alpha","MSE")
-print(alpha_mse)
+sel_alpha=which.min(c(mse0,mse1,mse2,mse3,mse4,mse5,mse6,mse7,mse8,mse9))-1
+sel_lambda=eval(parse(text=paste("fit",sel_alpha,"$lambda.1se",sep="")))
+fit_best_model=glmnet(indep_var,dep_var,alpha=sel_alpha/10,lambda=sel_lambda,family="gaussian")
